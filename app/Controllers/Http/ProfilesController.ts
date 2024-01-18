3037410bc0f3595199d0d1b3e60d317b270a7956
unknown <admin@salmanitb.com>
2024-01-19 00:22:57 +0700

    feat: add profile endpoints

---
+++ app/Controllers/Http/ProfilesController.ts
@@ -0,0 +1,83 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/PublicUser'
+import Profile from 'App/Models/Profile'
+import ProfileValidator from 'App/Validators/UpdateProfileValidator'
+
+export default class ProfilesController {
+  public async index({ request, response }: HttpContextContract) {
+    try {
+      const page = request.qs().page ?? 1
+      const perPage = request.qs().per_page ?? 10
+      const search = request.qs().search
+
+      const profiles = await Profile.query()
+        .select('name', 'gender', 'major')
+        .where('name', 'ILIKE', search ? '%' + search + '%' : '%%')
+        .orWhere('email', 'ILIKE', search ? '%' + search + '%' : '%%')
+        .preload('user')
+        .preload('university')
+        .preload('role')
+        .orderBy('name', 'asc')
+        .paginate(page, perPage)
+
+      return response.ok({
+        status: 'success',
+        messages: 'Get all profiles data success',
+        data: profiles,
+      })
+    } catch (error) {
+      return response.internalServerError({
+        status: error.status,
+        message: 'Get all profiles data failed',
+        error: error.message,
+      })
+    }
+  }
+
+  public async show({ params, response }: HttpContextContract) {
+    try {
+      const id: number = params.id
+      const userData: any = await User.find(id)
+      const profile: any = await Profile.query()
+        .select('*')
+        .where('user_id', id)
+        .preload('university')
+        .preload('role')
+        .preload('province')
+        .preload('regency')
+
+      return response.ok({
+        status: 'success',
+        message: 'Get user profile success',
+        data: { userData, profile },
+      })
+    } catch (error) {
+      return response.internalServerError({
+        status: error.status,
+        message: 'Get user profile failed',
+        error: error.message,
+      })
+    }
+  }
+
+  public async update({ params, request, response }: HttpContextContract) {
+    try {
+      const payload: any = await request.validate(ProfileValidator)
+      const id: number = params.id
+      const profile: any = await Profile.findByOrFail('user_id', id)
+      const updated: any = await profile.merge(payload).save()
+
+      return response.ok({
+        status: 'success',
+        message: 'Update user profile success',
+        data: updated,
+      })
+    } catch (error) {
+      return response.internalServerError({
+        status: error.status,
+        message: 'Update user profile failed',
+        error: error.message,
+      })
+    }
+  }
+}
---
+++ app/Models/Profile.ts
@@ -0,0 +1,101 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, hasOne, HasOne } from '@ioc:Adonis/Lucid/Orm'
+import University from './University'
+import Province from './Province'
+import Regency from './Regency'
+import Role from './Role'
+import User from './PublicUser'
+
+export default class Profile extends BaseModel {
+  public static table = 'profiles'
+
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public user_id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public gender: string
+
+  @column()
+  public phone: string
+
+  @column()
+  public line_id: string
+
+  @column()
+  public instagram: string
+
+  @column()
+  public province_id: number
+
+  @column()
+  public regency_id: number
+
+  @column.date()
+  public date_of_birthday: DateTime
+
+  @column()
+  public city_of_birth: string
+
+  @column()
+  public address: string
+
+  @column()
+  public university_id: number
+
+  @column()
+  public faculty: string
+
+  @column()
+  public major: string
+
+  @column()
+  public student_id: number
+
+  @column()
+  public intake_year: string
+
+  @column()
+  public role_id: number
+
+  @column()
+  public ssc: number
+
+  @column()
+  public lmd: number
+
+  @column()
+  public komprof: number
+
+  @column()
+  public spectra: number
+
+  @column()
+  public is_subscribing: boolean
+
+  @column.dateTime({ autoCreate: true, serializeAs: null })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true, serializeAs: null })
+  public updatedAt: DateTime
+
+  @hasOne(() => User)
+  public user: HasOne<typeof User>
+
+  @hasOne(() => University)
+  public university: HasOne<typeof University>
+
+  @hasOne(() => Province)
+  public province: HasOne<typeof Province>
+
+  @hasOne(() => Regency)
+  public regency: HasOne<typeof Regency>
+
+  @hasOne(() => Role)
+  public role: HasOne<typeof Role>
+}
